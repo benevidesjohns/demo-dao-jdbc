bbd134ba19fee86eff1743711c3163d066bf51f4
benevidesjohns <benevidesjohns@gmail.com>
2024-02-22 09:29:25 -0300

    feat: Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,22 @@
 package application;
 
+import java.time.LocalDate;
+import java.time.Month;
+
 import entities.Department;
+import entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Department department = new Department(1, "Books");
 		
-		System.out.println(department);
+		Seller seller = new Seller(
+				21, "Bob", "bob@gmail.com",
+				LocalDate.of(2000, Month.MARCH, 20), 
+				3000.0, department);
+		
+		System.out.println(seller);
 	}
 
 }
