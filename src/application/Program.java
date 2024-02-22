310f54ce2adc4b89d769a1d76e7e8d452cafe2e2
benevidesjohns <benevidesjohns@gmail.com>
2024-02-22 09:47:57 -0300

    feat: SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,6 +5,8 @@ import java.time.Month;
 
 import entities.Department;
 import entities.Seller;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 
 public class Program {
 
@@ -15,6 +17,8 @@ public class Program {
 				21, "Bob", "bob@gmail.com",
 				LocalDate.of(2000, Month.MARCH, 20), 
 				3000.0, department);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println(seller);
 	}
